664b4c67565153711da244963165765204e5f78f
Kienpt <kien.pt@kss.com.vn>
2023-05-31 09:40:15 +0700

    innit

---
+++ kienpt-util/src/main/java/com/kienpt/util/UtilApplication.java
@@ -0,0 +1,10 @@
+package com.kienpt.util;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.kienpt")
+public class UtilApplication {
+
+}
